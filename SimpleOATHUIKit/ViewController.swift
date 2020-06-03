faa478086d3629b3a285a56c75fc2fc3c4740c78
Jens Utbult <jens.utbult@yubico.com>
2020-06-03 10:46:57 +0200

    Initial Commit

---
+++ SimpleOATHUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleOATHUIKit
+//
+//  Created by Jens Utbult on 2020-06-03.
+//  Copyright © 2020 Jens Utbult. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
